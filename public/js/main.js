8b6cdf0d1c9233ee067130264e97223745bdb45c
Compelling Demigod <abhiitiwarii0@gmail.com>
2023-03-29 12:21:14 +0530

    .

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,4 @@
 $("button.btn-secondary").on("click", function () {
-  if ($("div#graph").css("visibility") == "visible") {
-    $("div#graph").css("visibility", "hidden");
-    $("button.btn-secondary").text("Display");
-  } else {
-    $("div#graph").css("visibility", "visible");
-    $("button.btn-secondary").text("Hide");
-  }
+    $("div#graph").slideToggle();
+  
 });

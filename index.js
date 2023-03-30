6dc36e589405578a70bd50f9526b614af9e684e7
01ABHAY10 <vishwakarmaabhay10@gmail.com>
2023-03-30 23:00:58 +0530

    Web3.Storage

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const  Web3Storage  = require('web3.storage');
+const express = require('express');
+const  {Web3Storage}  = require('web3.storage');
+const  {File}  = require('web3.storage')
+const https = require('https');
 
-
-
-const KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEQ5OTQ2YkM3ZDExNzU1ZTUxRWEyQUU4OEU3RjA1YjAwYkIyN2JlNzkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODAxNzIyNDU5NDcsIm5hbWUiOiJIQ1JNIn0.gDA83LOadNzAUVYV9s7H_gfFkDJBRYT85cR6cbmM49Y";
-const client = new Web3Storage({ token: KEY });
-
-const file = new File(['Healthcare Record management !'], 'project.txt', { type: 'text/plain' });
-async function upload(){
-  const cid = await client.put(file);
-  console.log('File stored with CID:', cid);
-}
-upload();
 
 const app = express();
 
+const KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEQ5OTQ2YkM3ZDExNzU1ZTUxRWEyQUU4OEU3RjA1YjAwYkIyN2JlNzkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODAxNzIyNDU5NDcsIm5hbWUiOiJIQ1JNIn0.gDA83LOadNzAUVYV9s7H_gfFkDJBRYT85cR6cbmM49Y';
+const client = new Web3Storage({ token: KEY });
+
+async function upload(){
+  const obj = { "name": "Healthcare record management", "type": "project","author" : "James Bond" };
+  const buffer = Buffer.from(JSON.stringify(obj))
+
+  const files = [ new File([buffer], 'trial.json')];
+  const cid = await client.put(files);
+  console.log("Stored JSON object with CID : "+ cid);
+}
+
+async function Retrieve (id,cid) {
+  const url = 'https://ipfs.io/ipfs/'+cid+'/'+id+'.json';
+  https.get(url,function(response){
+    response.on('data',function(data){
+      const obj = JSON.parse(data);
+      console.log(obj);
+    })
+  })
+}
+//  upload();
+  Retrieve('trial','bafybeicw5nzz3fgqhlhw36a2sin62i6rqiidesegirdo4si44dgy7w53ry');
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {

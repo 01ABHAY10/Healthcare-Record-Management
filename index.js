836e605a08efac39bddac7aed4432ab69509025d
01ABHAY10 <vishwakarmaabhay10@gmail.com>
2023-03-30 16:13:23 +0530

    web3

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const  Web3Storage  = require('web3.storage');
+
+
+
+const KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEQ5OTQ2YkM3ZDExNzU1ZTUxRWEyQUU4OEU3RjA1YjAwYkIyN2JlNzkiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2ODAxNzIyNDU5NDcsIm5hbWUiOiJIQ1JNIn0.gDA83LOadNzAUVYV9s7H_gfFkDJBRYT85cR6cbmM49Y";
+const client = new Web3Storage({ token: KEY });
+
+const file = new File(['Healthcare Record management !'], 'project.txt', { type: 'text/plain' });
+async function upload(){
+  const cid = await client.put(file);
+  console.log('File stored with CID:', cid);
+}
+upload();
 
 const app = express();
 

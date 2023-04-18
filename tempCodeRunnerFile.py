eae7d707c13a09bb1d4cb3c92c4935739c23a989
Compelling Demigod <abhiitiwarii0@gmail.com>
2023-04-18 17:40:33 +0530

    .

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,11 @@
-D:\Study\Healthcare record management
+
+#   data = csv.reader(file)
+#   for lines in data:
+#       diseaseCount = 0
+#       dat[lines[6]] += 1
+#       for i in range(6):
+#          if (lines[13+i] != ""):
+#             diseaseCount+=1
+#       disDat[diseaseCount]+=1;
+
+# print(disDat)

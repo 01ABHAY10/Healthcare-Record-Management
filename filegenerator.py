ccbbd9a83dc36b66176915759c1744358cb0843c
Compelling Demigod <abhiitiwarii0@gmail.com>
2023-04-18 17:40:29 +0530

    .

--- filegenerator.py
+++ filegenerator.py
@@ -3,12 +3,10 @@ import json
 import csv
 import sys
 
-# read JSON objects from standard input
 json_objs = []
 for line in sys.stdin:
     json_objs.append(json.loads(line))
 
-# append data from each JSON object to CSV file
 with open('event.csv', 'a', newline="") as f_object:
     writer_object = csv.writer(f_object)
     for json_obj in json_objs:
@@ -17,6 +15,5 @@ with open('event.csv', 'a', newline="") as f_object:
             data.append(json_obj[i])
         writer_object.writerow(data)
 
-# send success response to Node.js
 response = {'status': 'success'}
 sys.stdout.write(json.dumps(response))
--- main.py
+++ main.py
@@ -1,23 +1,47 @@
-
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
-import csv
 
 # Blood Group
-dat = {'A+':0, 'A-':0, 'B+':0,'B-':0, 'AB+':0, 'AB-':0, 'O+':0, 'O-':0, 'rh-null':0, 'others':0}
+dat = {
+    "A+": 0,
+    "A-": 0,
+    "B+": 0,
+    "B-": 0,
+    "AB+": 0,
+    "AB-": 0,
+    "O+": 0,
+    "O-": 0,
+    "rh-null": 0,
+    "others": 0,
+}
+disDat = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
 
-with open("event.csv", "r") as file:
-  data = csv.reader(file)
-  for lines in data:
-      dat[lines[6]] += 1
+df = pd.read_csv("event.csv")
 
-plt.bar(dat.keys(), dat.values(), width=0.9,autopct='%1.2f%%')
+for index, row in df.iterrows():
+    disease_count = 0
+    
+    for i in range(6):
+        if row[13 + i] != "nan":
+            disease_count += 1
+            
+    disDat[disease_count] += 1
+    dat[row[6]] += 1
+
+
+print(df.head())
+
+plt.bar(dat.keys(), dat.values(), width=0.9)
 
 save_path = "D:\Study\Healthcare record management\public\images\plot1.png"
 plt.savefig(save_path)
 plt.close()
-explodeTuple = (0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1)
-lab = ['A+', 'A-', 'B+','B-', 'AB+', 'AB-', 'O+', 'O-', 'rh-null', 'others']
-plt.pie(dat.values(), labels=lab, shadow=True, explode=explodeTuple,  autopct='%1.2f%%')
+
+
+explodeTuple = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
+lab = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-", "rh-null", "others"]
+plt.pie(dat.values(), labels=lab, shadow=True, explode=explodeTuple, autopct="%1.2f%%")
 save_path = "D:\Study\Healthcare record management\public\images\plot2.png"
 plt.savefig(save_path)
+plt.close()
